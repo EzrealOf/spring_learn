891a0e3d855e25e12b85c5ef434a7ce27e1cc615
黄佳明 <huangjiaming@souche.com>
2021-08-23 13:40:01 +0800

    feat:init

---
+++ spring-base/src/main/java/com/ezreal/learn/spring/base/SpringBaseApplication.java
@@ -0,0 +1,19 @@
+package com.ezreal.learn.spring.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created on 2021/8/19.
+ *
+ * @author Ezreal
+ * @since 2.0.0
+ */
+
+@SpringBootApplication
+public class SpringBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaseApplication.class, args);
+    }
+}
